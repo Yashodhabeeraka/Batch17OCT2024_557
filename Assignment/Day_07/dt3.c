e5b234d4c4f9ab1272889038e24add5b0d4d2356
Yashodhabeeraka <beerakayashodha@gmail.com>
2024-11-19 16:03:47 +0530

    Add files via upload

---
+++ Assignment/Day_07/dt3.c
@@ -0,0 +1,21 @@
+/* Description: To read a number in decimal (0-127) and display it in 2 digit hex character.
+Author: Yashodha_Beeraka_557
+Date of creation or Date of modification: 06-11-2024
+Version: 0.1v
+ */
+#include <stdio.h>
+
+int main()
+{
+    int decimal;
+
+    printf("Enter a decimal number (0-127): ");
+    scanf("%d", &decimal);
+    if (decimal < 0 || decimal > 127)
+        {
+        printf("Invalid input, Please enter a number between 0 and 127.\n");
+        return 1;
+    }
+    printf("Hexadecimal: %02X\n", decimal);
+    return 0;
+}
